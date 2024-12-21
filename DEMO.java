ad423f86f7d7cfb1ddb6e23dab9ab014adbaa98b
vicky-git-24 <vigneshmathiyazhagan99@gmail.com>
2024-12-21 11:06:54 +0530

    Update DEMO.java

--- DEMO.java
+++ DEMO.java
@@ -2,5 +2,6 @@ public class DEMO {
     public static void main(String[] args) {
         // Your code logic here
         System.out.println("Hello, World!"); // Example output
+        System.out.println("Hello, World!"); // Example output
     }
 }
